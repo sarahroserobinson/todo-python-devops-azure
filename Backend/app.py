1febc6c56c5141ba7dd44bddaeecc57dbfa40f1e
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2024-09-15 16:05:00 +0100

    adds POST

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,11 +6,13 @@ to_do_list = []
 
 @app.get("/")
 def get_list():
-    return to_do_list
+    return jsonify(to_do_list)
 
 @app.post("/")
 def add_to_do():
-    return
+    task = request.json.get('task')
+    to_do_list.append({'to_do': task})
+    return jsonify({'message': 'to do list updated'}), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
