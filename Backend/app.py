8db9055a70b28bdbeecc4b6ee8033ee5b3084184
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2024-09-15 15:56:13 +0100

    setting up api

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+to_do_list = []
+
 @app.get("/")
 def get_list():
-    return 
+    return to_do_list
+
+@app.post("/")
+def add_to_do():
+    return
+
+if __name__ == "__main__":
+    app.run(debug=True)
